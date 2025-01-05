5b1fbcc02b9b8c1afffd7402d7e013265380df9f
Vivek Kumar <kumarvivek7282827749@gmail.com>
2025-01-05 14:29:27 +0530

    Adde server url

--- client/src/url.js
+++ client/src/url.js
@@ -1,2 +1,4 @@
+// export const BASE_URL = "http://localhost:4000/api/auth";
+// export const SERVER_URL = "http://localhost:5000";
 export const BASE_URL = "http://localhost:4000/api/auth";
 export const SERVER_URL = "http://localhost:5000";
