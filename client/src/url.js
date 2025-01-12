db375a6bfb2c4cd9c9d9d207d1daf14d946ca792
Vivek Kumar <kumarvivek7282827749@gmail.com>
2025-01-12 16:19:23 +0530

    channged server url

--- client/src/url.js
+++ client/src/url.js
@@ -1,4 +1,5 @@
 // export const BASE_URL = "http://localhost:4000/api/auth";
 // export const SERVER_URL = "http://localhost:5000";
 export const BASE_URL = "http://88.222.213.217/api/auth";
-export const SERVER_URL = "http://88.222.213.217/";
+// export const SERVER_URL = "http://88.222.213.217/";
+export const SERVER_URL = "http://88.222.213.217:4001/";
